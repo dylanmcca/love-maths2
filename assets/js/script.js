ffa679c151a46bc2aa1e5bbc46215e54290588cb
dylanmcca <dylanmcca@gmail.com>
2025-09-08 21:06:57 +0100

    set up JS struction functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Connected!");
+function runGame() {
+    
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incrementScore(params) {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractionQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
